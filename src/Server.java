415b60fe9818124035ab64fccd4d7bc647bf2205
root <root@localhost.localdomain>
2017-12-02 12:51:42 -0500

    added class workable for thread

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,9 @@
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 
 public class Server implements Runnable{
     protected int          serverPortVal   = 8080;
@@ -54,5 +57,29 @@ public class Server implements Runnable{
             throw new RuntimeException("Not able to open the port 8080", e);
         }
     }
+
+public class WrkrRunnable implements Runnable{
+    protected Socket clntSocket = null;
+    protected String txtFrmSrvr   = null;
+ 
+    public WrkrRunnable(Socket clntSocket, String txtFrmSrvr) {
+        this.clntSocket = clntSocket;
+        this.txtFrmSrvr   = txtFrmSrvr;
+    }
+    public void run() {
+        try {
+            InputStream inputstrm  = clntSocket.getInputStream();
+            OutputStream outputstrm = clntSocket.getOutputStream();
+            long timetaken = System.currentTimeMillis();
+            outputstrm.write(("OK\n\nWrkrRunnable: " + this.txtFrmSrvr + " - " +timetaken +"").getBytes());
+            outputstrm.close();
+            inputstrm.close();
+            System.out.println("Your request has processed in time : " + timetaken);
+        } catch (IOException e) {           
+            e.printStackTrace();
+        }
+    }
+}
+
 
 }
